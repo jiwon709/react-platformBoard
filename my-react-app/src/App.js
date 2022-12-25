3471085d10192ef40f9eaf69b8cff03c6402d6d8
jiwon709 <jimmong8702@gmail.com>
2022-12-25 22:17:22 +0900

    add : App.js 에 커피주문 페이지, 메뉴주문 페이지 추가

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+
+import Coffee from './Coffee';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <header className='App-header'>
+          <Link to={'/Coffee'}>커피 주문</Link>
+          <Routes>
+            <Route path='/Coffee' element={ <Coffee/> } />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
